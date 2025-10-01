fa48df8adeb732e7ada387ff5d0d24194878085b
SalimTNKuho <salimalfaruq_2327240061@mhs.mdp.ac.id>
2025-10-01 12:31:29 +0700

    test002

--- P6/dashboard/app.js
+++ P6/dashboard/app.js
@@ -4,21 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-let products=[];
 
+let products=[];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Halaman daftar produk
 app.get('/minimarket', (req, res) => {
-    res.render('dashboard', { products });
+    res.render('minimarket', { products: products });
 });
 
 app.get('/add', (req, res) => {
     res.render('form');
 });
 
-// Form input produk
+// terima data dari form
 app.post('/add', (req, res) => {
     const{name, qty, price} = req.body;
     products.push({
@@ -29,3 +28,16 @@ app.post('/add', (req, res) => {
     app.redirect('/minimarket');
     // res.redirect('/minimarket');
 });
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+// });
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
