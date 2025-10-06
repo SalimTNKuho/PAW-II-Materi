c98e90ebe347988695bdada07950336ef19bf41f
SalimTNKuho <salimalfaruq_2327240061@mhs.mdp.ac.id>
2025-10-06 10:37:48 +0700

    post???

--- P7/dashboard/app.js
+++ P7/dashboard/app.js
@@ -3,12 +3,18 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// untuk parsing application/json
+app.use(express.urlencoded({ extended: true }));
+
 // Middleware untuk membaca file statis
 app.use(express.static(path.join(__dirname, 'views')));
 
 // Routing ke halaman utama (dashboard)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/tambah', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'tambah.html'));
 });
 
 // Menjalankan server
--- P7/dashboard/routes/index.js
+++ P7/dashboard/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tambah', function(req, res, next) {
+  res.render('tambah', { title: 'Express' });
+});
+
 module.exports = router;
